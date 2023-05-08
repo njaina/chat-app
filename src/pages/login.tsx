75a46f2eafc590317d6ddf333259fbd0945780d4
njaina <hei.njaina.2@gmail.com>
2023-05-08 23:24:05 +0300

    feat: update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,27 +1,24 @@
-import { useState, useEffect } from 'react';
-import {auth, google} from './conf'
-import { signInWithPopup } from 'firebase/auth';
-import Chat from './chat';
+import Head from 'next/head'
+import style from '../styles/Login.module.css'
+import Auth from './auth/googleAuth'
+import Form from './auth/form';
 
 
 const Login = ()=>{
-const [value,setValue] = useState<string|null>('');
-const manageClick = ()=>{
-    signInWithPopup(auth,google).then(data=>{
-    let email_data:string|null = data.user.email;
-    console.log(typeof email_data+"  ***********************")
-    setValue(email_data)
-    localStorage.setItem("email", email_data || '')
-    })
-}
-useEffect(()=>{
-    setValue(localStorage.getItem("email"))
-})
+
     return(
         <>
-        {value!==''?<Chat/>:
-        <button onClick={manageClick} >Click</button>}
-        </>
+        <Head>
+        <title>Chat App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className={style.container}>
+        <Form/>
+        <Auth/>
+        </main>
+      </>
+        
     )
 }   
 export default Login;
