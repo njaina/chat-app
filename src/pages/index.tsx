86ed1871cd228aafeb45191dfa0c283db2ca83e5
njaina <hei.njaina.2@gmail.com>
2023-06-09 06:47:44 +0300

    feat: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { Inter } from 'next/font/google'
-import { Main } from '../Components/Main/Main';
+import { useEffect } from 'react';
+import Router from 'next/router';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    null
-  )
+  useEffect(() => {
+    const userAuth = localStorage.getItem('email');
+    const userForm = localStorage.getItem('formData');
+
+    if (userAuth || userForm) {
+      Router.push('/profile');
+    } else {
+      Router.push('/login');
+    }
+  }, []);
+return null
 }
