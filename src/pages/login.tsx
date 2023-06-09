98089f84707f848fe45ad912beace64ef8e8816c
njaina <hei.njaina.2@gmail.com>
2023-06-09 06:45:28 +0300

    feat: update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,31 @@
-const Channel = () =>{
-    return(
-        <>
-        <h1> lgoin </h1>
-        </>
-    )
-}
-export default Channel;
+import { useRouter } from "next/router";
+import Form, {FormInputData} from "../Components/Components/auth/Form";
+import Auth from "../Components/Components/auth/GoogleAuth";
+import Head from "../Components/Components/Header/Header";
+
+const Login: React.FC = () => {
+
+  const history = useRouter();
+
+  const handleFormSubmit = (formData: FormInputData) => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+    history.push("/chat");
+  };
+
+  const handleAuthenticate = () => {
+    history.push("/chat");
+  };
+
+  return (
+    <>
+    
+      <Head title="Login" />
+      <main>
+        <Form onSubmit={handleFormSubmit} />
+        <Auth onAuthenticate={handleAuthenticate} />
+      </main>
+    </>
+  );
+};
+
+export default Login;
