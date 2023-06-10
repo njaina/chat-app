829983e9125d457498873ab59122a4b190968911
njaina <hei.njaina.2@gmail.com>
2023-06-10 04:12:24 +0300

    feat: update

--- src/pages/channel/create.tsx
+++ src/pages/channel/create.tsx
@@ -2,12 +2,16 @@ import Sidebar from "../../Components/features/Sidebar"
 import CreateChannelForm from "../../Components/logedIn/chat/CreateChannel"
 import Styles from '../../styles/Primary.module.css'
 import { createChannel } from "../../api/channel/createChannel"
+import { useState } from "react";
 
 export default function (){
+    const [createdChannelId, setCreatedChannelId] = useState('');
     const handleSubmit = async (formData) => {
         try {
+          const createdChannel = await createChannel(formData);
           await createChannel(formData);
           console.log('Canal créé avec succès');
+          setCreatedChannelId(createdChannel)
         } catch (error) {
           console.error('Erreur lors de la création du canal', error);
         }
@@ -17,6 +21,9 @@ export default function (){
         <Sidebar/>
         <h1>Channel create</h1>
         <CreateChannelForm onSubmit={handleSubmit}/>
+        {createdChannelId && (
+          <div>{createdChannelId}</div>
+        )}
         </main>
     )
 }
