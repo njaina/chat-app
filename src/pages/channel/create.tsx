36f8792d94df755ea916f52ec8813aab3737e88d
njaina <hei.njaina.2@gmail.com>
2023-06-10 03:44:13 +0300

    feat: update

--- src/pages/channel/create.tsx
+++ src/pages/channel/create.tsx
@@ -1,5 +1,11 @@
+import Sidebar from "../../Components/features/Sidebar"
+import Style from '../../styles/Primary.module.css'
+
 export default function (){
     return(
+        <main className={Style.FlexContainer} >
+        <Sidebar/>
         <h1>Channel create</h1>
+        </main>
     )
 }
