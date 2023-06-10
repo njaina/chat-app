5a4341cbfb9c7cb8e73531660484eebcd05495f2
njaina <hei.njaina.2@gmail.com>
2023-06-10 03:57:31 +0300

    feat: update

---
+++ src/api/channel/createChannel.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import { API_URL } from '../API_URL';
+
+export const createChannel = async (formData) => {
+  try {
+    const response = await axios.post(`${API_URL}/channel/create`, formData);
+    return response.data;
+  } catch (error) {
+    throw new Error('Erreur lors de la création du canal');
+  }
+};
