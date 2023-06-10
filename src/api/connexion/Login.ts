f5ab1b6a7226e2c91c901f1d365b10bdff12f5cf
njaina <hei.njaina.2@gmail.com>
2023-06-10 05:28:19 +0300

    feat: update

--- src/api/connexion/Login.ts
+++ src/api/connexion/Login.ts
@@ -3,7 +3,7 @@ import { API_URL } from '../API_URL';
 
 export const loginUser = async (userCredentials) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, userCredentials);
+    const response = await axios.post(`${API_URL}/users/login`, userCredentials);
     return response.data;
   } catch (error) {
     throw new Error("Erreur lors de la connexion de l'utilisateur");
